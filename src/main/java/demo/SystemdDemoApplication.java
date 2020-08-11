9a268e4793f705f573f0daa3bc2e5640e93c02cf
Vedran Pavic <vedran.pavic@gmail.com>
2020-08-11 22:52:25 +0200

    Initial commit

---
+++ src/main/java/demo/SystemdDemoApplication.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SystemdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemdDemoApplication.class, args);
+    }
+
+    @GetMapping(path = "/")
+    public String home() {
+        return "Hello World!";
+    }
+
+}
